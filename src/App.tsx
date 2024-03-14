97e7e03ade2a133014b24dc16ab2f3d7e4304e59
samfrexz619 <samfrexz69@gmail.com>
2024-03-14 19:01:02 +0100

    add comps

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Checkout from './components/checkout/Checkout'
+
+function App() {
+
+  return (
+    <main>
+      <Checkout />
+    </main>
+  )
+}
+
+export default App
